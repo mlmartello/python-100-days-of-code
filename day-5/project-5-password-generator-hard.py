963ee40a1e0e1db0e2a057f9b75ac90fc0d4af95
Matheus Luiz Martello <matheusmartello21@gmail.com>
2026-08-10 11:55:27 -0300

    Clarify hard version password generation comment
    Updated comment for clarity on password generation.

--- day-5/project-5-password-generator-hard.py
+++ day-5/project-5-password-generator-hard.py
@@ -9,11 +9,10 @@ nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
 # HARD VERSION:
-# When you've completed the easy version, you're ready to tackle the hard version. In the advanced version of this project the final password does not follow a pattern. So the example above might look like this:
+# When you've completed the easy version, you're ready to tackle the hard version. In the advanced version of this project, the final password does not follow a pattern. So the example above might look like this:
 # x$d24g*f9
 # And every time you generate a password, the positions of the symbols, numbers, and letters are different. This will make the password more difficult for hackers to crack.
 # The essential skill of a good programmer is using Google to find what you need. Your brain is for thinking, not memorising functions! You will need to Google to solve this project on the hard level. 
-# If you get stuck, check the hint below for what to Google.
 
 password_list = []
 for char in range(0, nr_letters):
